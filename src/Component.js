1eb3a0276744b1ee200e7404dd5afa1073ce831f
rabishh <rabishyoung@gmail.com>
2022-02-13 13:41:54 +0530

    lata ji wiki

---
+++ src/Component.js
@@ -0,0 +1,26 @@
+import React from "react"
+function Components(){
+    return (
+     <div className="main">
+        <header>
+            <h1>Lata Mankeskar</h1>
+            <p>The Women Who is Queen of Voice</p>
+        </header>
+        <div className="img">
+        <img src="https://spiderimg.amarujala.com/cdn-cgi/image/width=674,height=379.25,fit=cover,f=auto/assets/images/2020/09/28/750x506/lata-mangeshkar_1601279835.jpeg" />
+        <h3><span>Lata-Mankeskar  ::</span> <span>1929-2022</span></h3>
+        </div>
+        <div className="content">
+            <ul>
+                <li>Early Life:<p>Mangeshkar was born in a Maharashtrian Brahmin family[15] in 1929, the eldest daughter of Deenanath Mangeshkar, a Marathi and Konkani musician, and his wife Shevanti[16] in Indore (in the present-day Indore district of Madhya Pradesh and then the capital of the princely state of Indore which was part of the Central India Agency in British India). Her father, Deenanath Mangeshkar, was a classical singer and theatre actor. Her mother, Shevanti (later renamed Shudhamati), a Gujarati woman from Thalner, Bombay Presidency (now in northwest Maharashtra), was Deenanath's second wife; his first wife Narmada, who had died, was Shevanti's older sister.[17]</p></li>
+                <li>Singing Career<p>In 1942, when Mangeshkar was 13, her father died of heart disease.[23] Master Vinayak (Vinayak Damodar Karnataki), the owner of Navyug Chitrapat movie company and a close friend of the Mangeshkar family, took care of them. He helped her get started in a career as a singer and actress.[24]</p></li>
+                <li>Bangali Career <p>Mangeshkar has sung 185 songs in Bengali,[6] making her debut in 1956 with the hit song "Aakash Prodeep Jwole", composed by Satinath Mukhopadhyay.[133] The same year, she recorded "Rongila Banshite", composed by Bhupen Hazarika, which was also a hit.[134] In the late 1950s, she recorded a string of hits such as "Jaare Ude Jare Pakhi", "Na Jeona", and "Ogo Aar Kichu To Noy", all composed by Salil Chowdhury, and which were respectively adapted into Hindi as "Ja Re Ud Ja Re Panchi" and "Tasveer Tere Dil Mein" in Maya, and "O Sajna" in Parakh.[135] In 1960, she recorded "Akash Pradip Jole".[136] Later in the 1960s, she sang hits like "Ekbar Biday De Ma Ghure Ashi," "Saat Bhai Champa," "Ke Pratham Kache Esechi," "Nijhum Sandhyay," "Chanchal Mon Anmona," "Asharh Srabon," "Bolchi Tomar Kaney," and "Aaj Mon Cheyeche" by composers like Sudhin Dasgupta, Hemant Kumar and Salil Chowdhury</p></li>
+            </ul>
+        </div>
+        <div className="link">
+            <h5>if you have time , you should read more about this incridible human being on his </h5><a href="https://en.wikipedia.org/wiki/Lata_Mangeshkar">WIKI</a>
+        </div>
+     </div>
+    )
+}
+export default Components;
